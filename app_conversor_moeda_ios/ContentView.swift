fab92521a7df9bcee37ab3bc1b3e0b953046120c
Fernando Pereira <ferpereira1@gmail.com>
2024-09-10 14:57:19 -0300

    Initial Commit

---
+++ app_conversor_moeda_ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  app_conversor_moeda_ios
+//
+//  Created by usuario on 10/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app_conversor_moeda_ios/app_conversor_moeda_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_conversor_moeda_iosApp.swift
+//  app_conversor_moeda_ios
+//
+//  Created by usuario on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct app_conversor_moeda_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
